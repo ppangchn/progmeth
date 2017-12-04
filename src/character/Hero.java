f30f8c6146bf7c9f8ed934bbc952fa9abb033fe5
toeiabe <31318003+toeiabe@users.noreply.github.com>
2017-12-04 21:46:55 +0700

    toei1

--- src/character/Hero.java
+++ src/character/Hero.java
@@ -14,14 +14,17 @@ public class Hero implements IRenderable {
 	private int exp = 0;
 	private int lv = 1;
 	private int maxexp = 50;
-	private int way = 0;
+	private int time = 0;
+	private String[] left = {"left.png","left2.png","left3.png"};
+	private String[] right = {"right.png","right2.png","right3.png"};
+	private String[] front = {"front.png","front2.png","front3.png"};
+	private String[] back = {"back.png","back2.png","back3.png"};
 	
 	public Hero() {
 		this.x = 400;
 		this.y = 225;
 		setHero();
 	}
-	
 	public boolean isDead() {
 		if (life==0) return true;
 		return false;
@@ -31,7 +34,7 @@ public class Hero implements IRenderable {
 	}
 	public void decreaseLife() {
 		life--;
-		//ให้กลับมาอยู่ที่ตำแหน่งตรงกลาง
+		
 	}
 	public void randomItem() {
 		
@@ -53,48 +56,39 @@ public class Hero implements IRenderable {
 		this.y = y;
 	}
 	public void setHero() {
-		heropic = new Image("cloudleft.png");
+		heropic = new Image("front.png");
 	}
-
-	@Override
-	public int getZ() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 	@Override
 	public void draw(GraphicsContext gc) {
-		// TODO Auto-generated method stub
 		System.out.println("hero");
+		time++;
+		if(time>=30) time = 0; 
 		gc.drawImage(heropic, x, y);
+		
 		
 	}
 	public void updatePos(String control) {
 		if (control.contains("a")) if(x>=0) {
 			x-=4;
-			heropic = new Image("cloudleft.png");
-			way = 0;
+			heropic = new Image(left[time/10]);
 		}
 		if (control.contains("d")) if (x+90<=800) {
-			x+=4; //subtract length pic duay
-			heropic = new Image("cloudright.png");
-			way = 1;
+			x+=4; 
+			heropic = new Image(right[time/10]);
+			
 		}
 		if (control.contains("w")) if (y-4>=0) {
 			y-=4;
-			if (way==1) heropic = new Image("cloudupright.png");
-			else heropic = new Image("cloudupleft.png");
+			heropic = new Image(back[time/10]);
+			
 		}
 		if (control.contains("s")) if (y+90<=450) {
 			y+=4;
-			heropic = new Image("cloudleft.png");
-			if (way==1) heropic = new Image("clouddownright.png");
-			else heropic = new Image("clouddownleft.png");
+			heropic = new Image(front[time/10]);
 		}
 	}
 	@Override
 	public boolean isVisible() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
