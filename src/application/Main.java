559206527af6f5e6beab7df0a4d5586ee8c988fb
ppangchn <pangpy_pang@hotmail.com>
2017-12-05 15:21:41 +0700

    AA

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,16 +7,11 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	public  AudioClip sound;
-	public static boolean stopMusic =  false;
 	@Override
 	public void start(Stage primaryStage) {
 		StartWindow s = new StartWindow(primaryStage);
-		AudioClip sound = new AudioClip(ClassLoader.getSystemResource("Hello.mp3").toString());
-		sound.play();
 		s.drawStartWindow();
 		primaryStage.show();
-		if(stopMusic) sound.stop();
 		
 	}
 	
